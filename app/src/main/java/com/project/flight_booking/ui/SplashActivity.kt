730372cbf1ffe089d5f3b31071d40b723ac4f9ef
bouzayani2010 <kacem.bouzayani@gmail.com>
2023-10-06 23:38:56 +0100

    add splash screen

---
+++ app/src/main/java/com/project/flight_booking/ui/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.project.flight_booking.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.project.flight_booking.R
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT: Long = 2000 // 2 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        Handler().postDelayed({
+            // Start your main activity after the splash screen duration
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
