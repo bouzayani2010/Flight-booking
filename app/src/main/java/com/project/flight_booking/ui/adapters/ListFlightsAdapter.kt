4076cf930a51247802f3d2d8d614d0b0804bea16
bouzayani2010 <kacem.bouzayani@gmail.com>
2023-10-07 18:49:21 +0100

    init detail View

--- app/src/main/java/com/project/flight_booking/ui/adapters/ListFlightsAdapter.kt
+++ app/src/main/java/com/project/flight_booking/ui/adapters/ListFlightsAdapter.kt
@@ -1,9 +1,11 @@
 package com.project.flight_booking.ui.adapters
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.project.flight_booking.R
 import com.project.flight_booking.model.Trip
@@ -14,10 +16,11 @@ import java.time.Duration
 import java.time.format.DateTimeFormatter
 
 class ListFlightsAdapter(private val items: List<Trip>,private val  viewModel: MainViewModel) : RecyclerView.Adapter<ListFlightsAdapter.ViewHolder>() {
-
+    @RequiresApi(Build.VERSION_CODES.O)
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var pattern: String?="yyyy-MM-dd'T'HH:mm:ss'+00:00'"
         var formatter = DateTimeFormatter.ofPattern(pattern)
+
         fun bind(item: Trip, viewModel: MainViewModel) {
             itemView.findViewById<TextView>(R.id.airlineName).text = item.airline.name
             itemView.findViewById<TextView>(R.id.price).text = item.price
@@ -41,11 +44,13 @@ class ListFlightsAdapter(private val items: List<Trip>,private val  viewModel: M
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_trip, parent, false)
         return ViewHolder(view)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(items[position],viewModel)
     }
--- app/src/main/java/com/project/flight_booking/ui/fragment/DetailFlightFragment.kt
+++ app/src/main/java/com/project/flight_booking/ui/fragment/DetailFlightFragment.kt
@@ -1,16 +1,77 @@
 package com.project.flight_booking.ui.fragment
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.ViewModelProvider
 import com.project.flight_booking.R
+import com.project.flight_booking.apis.RetrofitClient
+import com.project.flight_booking.model.Trip
+import com.project.flight_booking.repositry.MainRepositry
+import com.project.flight_booking.viewmodels.MainViewModel
+import com.project.flight_booking.viewmodels.MainViewModelFactory
+import java.time.Duration
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 class DetailFlightFragment : Fragment() {
 
+    private lateinit var viewModel: MainViewModel
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        val repository = MainRepositry(RetrofitClient.apiService)
+        val viewModelFactory = MainViewModelFactory(repository)
+        viewModel =
+            ViewModelProvider(requireActivity(), viewModelFactory).get(MainViewModel::class.java)
+
+        setObservers()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setObservers() {
+        viewModel.selectedtrip.value?.let {item->
+           refreshView(item)
+
+        }
+        viewModel.selectedtrip.observe(this){
+            refreshView(it)
+        }
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun refreshView(item: Trip) {
+        var pattern: String?="yyyy-MM-dd'T'HH:mm:ss'+00:00'"
+        var formatter = DateTimeFormatter.ofPattern(pattern)
+        view?.let {
+            it.findViewById<TextView>(R.id.airlineName)?.text = item.airline.name
+            it.findViewById<TextView>(R.id.price).text = item.price
+
+            val departureDate: LocalDate = LocalDate.parse(item.departure.scheduled, formatter)
+            val arriveDate: LocalDate = LocalDate.parse(item.arrival.scheduled, formatter)
+            it.findViewById<TextView>(R.id.departureDate).text = departureDate.toString()
+            it.findViewById<TextView>(R.id.arriveDate).text = arriveDate.toString()
+
+            val departureTime: LocalTime = LocalTime.parse(item.departure.scheduled, formatter)
+            val arriveTime: LocalTime = LocalTime.parse(item.arrival.scheduled, formatter)
+            it.findViewById<TextView>(R.id.departureTime).text = departureTime.toString()
+            it.findViewById<TextView>(R.id.arriveTime).text = arriveTime.toString()
+
+            val duration = Duration.between(departureTime,arriveTime)
+
+            it.findViewById<TextView>(R.id.duration).text =""+ duration.toHours()+"H,"+ duration.toMinutes()+"m"
+
+        }
     }
 
     override fun onCreateView(
@@ -18,7 +79,9 @@ class DetailFlightFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_flight, container, false);
+       val view:View = inflater.inflate(R.layout.fragment_detail_flight, container, false);
+
+        return view
     }
 
     companion object {
